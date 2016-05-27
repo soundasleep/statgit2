bba92b4d3392520fc9ae12ab290ee2bffba1ddf2
Jevon Wright <jevon@powershop.co.nz>
2016-05-27 16:47:10 +1200

    Adding link to authors.html

--- generate.rb
+++ generate.rb
@@ -1,10 +1,9 @@
 #!/bin/ruby
 
 require_relative "lib/preload"
-repository_url = options[:url]
 
 # Do all the things
-repository = CreateRepository.new(url: repository_url, options: options).call
+repository = CreateRepository.new(options: options).call
 
 AnalyseRepository.new(repository: repository, options: options).call
 GenerateStatistics.new(repository: repository, options: options).call
