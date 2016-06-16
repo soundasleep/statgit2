453411872b9239c1b6d91c9ed2cce950f22101eb
Jevon Wright <jevon@powershop.co.nz>
2016-06-16 16:49:01 +1200

    Fix Revision count always returning 1

--- models/commit_file.rb
+++ models/commit_file.rb
@@ -3,6 +3,13 @@ class CommitFile < ActiveRecord::Base
   has_one :commit_diff, dependent: :destroy
 
   def revisions
-    @revisions ||= CommitDiff.where(commit_file: self).count
+    @revisions ||= commit.repository.commits.map do |commit|
+      file = commit.select_file(full_path)
+      if file
+        commit.commit_diffs.where(commit_file: file).count
+      else
+        0
+      end
+    end.sum
   end
 end
