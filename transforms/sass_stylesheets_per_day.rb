36e3b7bf8ab88de4dd1bf0c8000c11bc8288e9c5
Jevon Wright <jevon@jevon.org>
2018-05-16 13:29:59 +1200

    Remove crashing preload

--- transforms/sass_stylesheets_per_day.rb
+++ transforms/sass_stylesheets_per_day.rb
@@ -8,7 +8,7 @@ class SassStylesheetsPerDay
   end
 
   def call
-    raw = repository.daily_commits.preload(:file_sass_stylesheets).map do |commit|
+    raw = repository.daily_commits.map do |commit|
       [ iso_date(commit.date), commit.file_sass_stylesheets_count ]
     end
 
