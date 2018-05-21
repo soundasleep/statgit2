ec60750e1de87b022d8d143e651dd71a5348fe11
Jevon Wright <jevon@jevon.org>
2018-05-21 12:13:07 +1200

    Remove broken preload

--- transforms/sass_properties_per_rule_per_day.rb
+++ transforms/sass_properties_per_rule_per_day.rb
@@ -8,7 +8,7 @@ class SassPropertiesPerRulePerDay
   end
 
   def call
-    raw = repository.daily_commits.preload(:file_sass_stylesheets).map do |commit|
+    raw = repository.daily_commits.map do |commit|
       [ iso_date(commit.date), commit.sass_properties.to_f / commit.sass_rules.to_f ]
     end
 
